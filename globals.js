79940f6e067e7f3ceff48576da2058a17258b217
Gajendra Bhamare <gajendrabhamare5@gmail.com>
2024-03-28 11:50:29 +0530

    first commit

---
+++ globals.js
@@ -0,0 +1,15 @@
+//Globals windows
+
+//__dirname = path to current directory
+//__filename = file name
+// require = function to use moduls (Comman Js)
+// module = info about current module (file)
+// process = info about env where the program is being execute
+
+console.log(__dirname);
+
+setInterval(()=>{
+
+console.log('Hello World');
+
+},1000);
---
+++ modules.js
@@ -0,0 +1,14 @@
+// Modules
+// every file is module (by default)
+// Modules - encapsulated code (only share minimum )
+
+const gajendra = 'gajendra';
+const durgesh = 'durgesh';
+
+const sayhii = (name) => {
+console.log(`Hello there ${name}`);
+}
+
+sayhii ('poonam');
+sayhii (gajendra);
+sayhii (durgesh);
