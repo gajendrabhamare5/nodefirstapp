da0eb373dc05b0a2f56afb59e76bb9b1eb689ea8
Gajendra Bhamare <gajendrabhamare5@gmail.com>
2024-04-01 12:35:49 +0530

    second commit

--- app.js
+++ app.js
@@ -1,44 +1,9 @@
-/* const { readFileSync, writeFileSync} = require('fs');
+const EventEmitter = require('events')
 
-const first = readFileSync('./content/first.txt','utf8')
-const second = readFileSync('./content/second.txt','utf8')
+const customEmitter =  new EventEmitter()
 
-console.log(first,second);
+customEmitter.on('response',() => {
+    console.log('data received')
+})
 
-writeFileSync('./content/result-sync.txt',`here is the result :  ${first}, ${second}`,{flag:'a'}) */
-
-// http server
-
-/* const http = require('http');
-const server = http.createServer((req,res)=>{
-     console.log(req)
-    res.write('Welcome to our home page')
-    res.end()
-
-if (req.url === '/') {
-    res.end('Welcome to our page');
-}
-else if (req.url === '/about') {
-    res.end('here is our history... ');
-}else{
-    res.writeHead(404, {'Content-Type': 'text/html'});
-req.end(`
-<1>OOPS!!</h1>
-<p>we cant see tot he page you are looking for</p>
-<a href="/">back home</a>
-`);
-}
-});
-
-server.listen(5000, () => {
-    console.log('Server is running on port 5000');
-}); */
-
-
-// npm start
-
-const _ = require('lodash')
-
-const items = [1,[2,[3,[4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+customEmitter.emit('response')
