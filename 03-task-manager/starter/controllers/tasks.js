c763b653d6d84e5e274279b0dfc2a43e032886ba
Gajendra Bhamare <gajendrabhamare5@gmail.com>
2024-04-04 13:04:24 +0530

    .

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -55,7 +55,10 @@ const deleteTask = async (req,res) =>{
 const updateTask = async (req,res) =>{
     try {
         const {id:taskID} = req.params
-        const task = await task.findOneAndUpdate({_id:taskID},req.body)
+        const task = await task.findOneAndUpdate({_id:taskID},req.body,{
+            new:true,
+            runValidators:true
+        })
 
         if(!task){
             return res.status(404).json({msg:`No task with id : ${taskID}`})
@@ -63,7 +66,7 @@ const updateTask = async (req,res) =>{
 
         res.status(200).json({ task })
     } catch (error) {
-
+        res.status(500).json({ msg:error })
     }
 }
 
