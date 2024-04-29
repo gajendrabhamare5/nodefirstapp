50e8fbaa475e8b8cd8893fb808aee27ebaae61c2
Gajendra Bhamare <gajendrabhamare5@gmail.com>
2024-04-29 17:43:20 +0530

    file-upload

--- 07-file-upload/starter/controllers/productController.js
+++ 07-file-upload/starter/controllers/productController.js
@@ -1,9 +1,12 @@
+const { StatusCodes } = require('http-status-codes');
 const Product = require('../models/Product')
 const {statusCodes} = require('http-status-code')
 
 const createProduct = async (req,resp)=>{
     // resp.send('create product')
     console.log(req.body);
+    const product = await Product.create(req.body)
+    res.statusCodes(StatusCodes.CREATED).json({ product })
 }
 const getAllProduct = async (req,resp)=>{
     resp.send('list of all products')
