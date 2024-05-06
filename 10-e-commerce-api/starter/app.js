c632a862212bddc02f28ca8679955d7308872cd9
Gajendra Bhamare <gajendrabhamare5@gmail.com>
2024-05-06 18:02:01 +0530

    e-commerce

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,1 +1,39 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+require('express-async-errors')
+
+//express
+const express = require('express')
+const app = express()
+
+//Database
+const connectDB = require('./db/connect')
+
+//middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.use(express.json())
+
+app.get('/',(req,resp) =>{
+    resp.send('e-commerce api')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
+const port = process.env.PORT || 5000
+
+const start = async () =>{
+
+try {
+    await connectDB(process.env.url)
+    app.listen(port,console.log(`Server is listening on port ${port}...`))
+} catch (error) {
+    console.log(error);
+}
+};
+
+
+
+start();
--- 10-e-commerce-api/starter/db/connect.js
+++ 10-e-commerce-api/starter/db/connect.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose');
 
 const connectDB = (url) => {
+
   return mongoose.connect(url);
 };
 
+
 module.exports = connectDB;
+
+/* const connectDB = (url) => {
+
+  return mongoose.connect(url)
+    .then(() => {
+      console.log('Connected to database');
+    })
+    .catch((error) => {
+      console.error('Error connecting to database:', error);
+      throw error;
+    });
+};
+
+module.exports = connectDB; */
