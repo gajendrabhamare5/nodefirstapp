073728065553ca73e2ebe0f7e9da49023c577ca2
Gajendra Bhamare <gajendrabhamare5@gmail.com>
2024-05-07 16:54:02 +0530

    e-commerce-api

--- 10-e-commerce-api/starter/controllers/authControllers.js
+++ 10-e-commerce-api/starter/controllers/authControllers.js
@@ -1,8 +1,7 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors');
-const jwt = require('jsonwebtoken')
-
+const {createJWT} = require('../utils')
 const register = async (req, res) => {
     /* res.send('Register User') */
 
@@ -18,7 +17,8 @@ const register = async (req, res) => {
 
     const user = await User.create(req.body)
     const tokenUser = { name: user.name, userId: user._id, role: user.role }
-    const token = jwt.sign(tokenUser,'jwtSecret',{expiresIn:'1d'})
+    /* const token = jwt.sign(tokenUser,'jwtSecret',{expiresIn:'1d'}) */
+    const token = createJWT({payload:tokenUser})
 
     res.status(StatusCodes.CREATED).json({ user:tokenUser,token })
 };
