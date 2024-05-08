2ffa02e3bddd9ad7bf48c4382db447eecdd2d811
Gajendra Bhamare <gajendrabhamare5@gmail.com>
2024-05-08 17:58:26 +0530

    e-commerce-apis

--- 10-e-commerce-api/starter/controllers/authControllers.js
+++ 10-e-commerce-api/starter/controllers/authControllers.js
@@ -1,12 +1,11 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors');
-const {attachCookiesToResponse} = require('../utils');
+const { attachCookiesToResponse,createTokenUser } = require('../utils');
 
 const register = async (req, res) => {
-    /* res.send('Register User') */
 
-    const { email } = req.body
+    const { email,name, password } = req.body;
     const emailAlreadyExists = await User.findOne({ email })
     if (emailAlreadyExists) {
         throw new CustomError.BadRequestError('Email Already exists')
@@ -16,47 +15,43 @@ const register = async (req, res) => {
     const isFirstAccount = (await User.countDocuments({})) === 0;
     const role = isFirstAccount ? 'admin' : 'user';
 
-    const user = await User.create(req.body)
-    const tokenUser = { name: user.name, userId: user._id, role: user.role }
+    const user = await User.create(name, email, password, role)
+    const tokenUser = createTokenUser(user);
 
-    attachCookiesToResponse({res,user:tokenUser})
+    attachCookiesToResponse({ res, user: tokenUser })
     res.status(StatusCodes.CREATED).json({ user: tokenUser })
-    /* const token = jwt.sign(tokenUser,'jwtSecret',{expiresIn:'1d'}) */
-    /* const token = createJWT({payload:tokenUser}) */
-
 
 };
+
 const login = async (req, res) => {
-    const {email,password} = req.body
 
-if(!email || !password){
-    throw new CustomError.BadRequestError('Please provide email and password')
-}
+    const { email, password } = req.body
+    if (!email || !password) {
+        throw new CustomError.BadRequestError('Please provide email and password')
+    }
+    const user = await User.findOne({ email })
 
-const user = await User.findOne({email})
+    if (!user) {
+        throw new CustomError.BadRequestError('Invalid credentials')
+    }
+    const isPasswordCorrect = await user.comparePassword(password)
 
-if(!user){
-    throw new CustomError.BadRequestError('Invalid credentials')
-}
-
-const isPasswordCorrect = await user.comparePassword(password)
-if(!isPasswordCorrect){
-    throw new CustomError.BadRequestError('Invalid credentials')
-}
-
-const tokenUser = { name: user.name, userId: user._id, role: user.role }
-
-attachCookiesToResponse({res,user:tokenUser})
-res.status(StatusCodes.CREATED).json({ user: tokenUser })
+    if (!isPasswordCorrect) {
+        throw new CustomError.BadRequestError('Invalid credentials')
+    }
+    const tokenUser = createTokenUser(user);
+    attachCookiesToResponse({ res, user: tokenUser })
+    res.status(StatusCodes.OK).json({ user: tokenUser })
     // res.send('Login User')
 
 };
+
 const logout = async (req, res) => {
-    res.cookie('token','logout',{
-        httpOnly:true,
-        expires: new Date(Date.now())
+    res.cookie('token', 'logout', {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000)
     })
-    res.status(StatusCodes.OK).json({msg:'user log out!'})
+    res.status(StatusCodes.OK).json({ msg: 'user log out!' })
 };
 
 module.exports = {
--- 10-e-commerce-api/starter/utils/jwt.js
+++ 10-e-commerce-api/starter/utils/jwt.js
@@ -1,10 +1,6 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-/* const { token } = require('morgan'); */
-
 const createJWT = ({ payload }) => {
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
-
         expiresIn: process.env.JWT_LIFETIME,
     });
     return token;
@@ -12,20 +8,18 @@ const createJWT = ({ payload }) => {
 
 const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 
-const attachCookiesToResponse = ({res,user}) => {
-    const token = createJWT({payload:user})
+const attachCookiesToResponse = ({ res, user }) => {
+    const token = createJWT({ payload: user })
 
     const oneDay = 1000 * 60 * 60 * 24
 
-    res.cookie('token',token,{
-        httpOnly:true,
-        expires:new Date(Date.now()+ oneDay),
-        secure:process.env.NODE_ENV === 'production',
-        signed:true,
-    })
-
-    res.status(201).json({ user })
-
+    res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + oneDay),
+        secure: process.env.NODE_ENV === 'production',
+        signed: true,
+    });
+    /*  res.status(201).json({ user }) */
 }
 
 module.exports = {
